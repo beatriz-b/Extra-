bb9d1c0adf5841f660a35481bd11333ae8af056f
beatriz-b <54721326+beatriz-b@users.noreply.github.com>
2019-11-21 22:04:32 -0800

    Update and rename 10.18 to 10_18.cpp

--- 10_18.cpp
+++ 10_18.cpp
@@ -11,7 +11,25 @@
 using namespace std;
 
 // 11- number of strings 100- max string size 
+// List the contact list 
 char phoneBook [11][100] = {"Alejandra Cruz, 555-1223", "Joe Looney, 555-0097",
 				"Geri Palmer, 555-8787", "Li Chen, 555-1212", "Holly Gaddis, 555-8878",
 				"Sam Wiggis, 55-0998", "Bob Kain, 555-8712","Tim Haynes, 555-7676",
 				"Warren Gaddis, 555-9037", "Jean James, 555-4939", "Ron Palmer, 555-2783"}
+				
+int main (){
+
+int i = 0;
+length = 100; 
+
+cout << "To search for your contact's number please enter a name or partial name of the person.\n";
+
+	cin.getline(search, length);
+	
+// i know I have to create a while loop that searches through the names but i don't know how to 
+// set its parameters 
+
+// I also can't find a funciton that returns the whole string "find.()" and function like it only 
+//return the index position the input matches
+
+} 
